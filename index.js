57cd59fd3c233affae7b164e01e54767bec27ba4
arthur <88572711+arthurbuenodasilva@users.noreply.github.com>
2025-04-01 21:11:19 -0300

    Add files via upload
    conexao com o banco

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 //biblioteca para criar api
 import express from "express";
+import { Sequelize } from "sequelize";
+
+
+const sequelize = new Sequelize('postgres', 'postgres', '123456', {
+    host: 'localhost',
+   
+    dialect: 'postgres',
+    define:{
+        timestamps:false,
+        freezeTableName:true
+    }
+  });
+
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+
 
 const app = express();
 
@@ -15,4 +35,7 @@ app.get('/teste', (request,response)=>{
 
 //porta e mensagem de inicio 
 app.listen(3000,()=>{console.log('Servidor rodando ...')});
+
+// sequelize - npm install sequelize pg pg-hstore ( instalador do interpretador de sql);
+
 
